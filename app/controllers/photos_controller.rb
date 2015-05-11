d29410ba2249bd7e6d27e8b50c0d84f475d5aaa6
ddepetris2016 <ddepetris2016@kellogg.northwestern.edu>
2015-05-11 15:34:05 -0500

    create_read_delete_complete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,39 @@ class PhotosController < ApplicationController
   end
 
 
+  def show
+
+    @id_num = params[:id]
+    a = Photo.find_by({:id => @id_num})
+    @caption = a.caption
+    @image = a.source
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+
+    a=Photo.new
+    a.source = params[:the_source]
+    a.caption = params[:the_caption]
+    a.save
+
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+  def destroy
+
+    a = Photo.find(params[:id])
+    a.destroy
+
+    redirect_to("http://localhost:3000/photos")
+
+  end
+
+
 end
+
